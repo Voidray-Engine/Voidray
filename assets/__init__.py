4067ac3047d3b962d9cf108127aba1c6b3413c2a
VoidRay Team <sunthekitsune@gmail.com>
2025-06-11 19:08:02 +0200

    Delete assets directory

--- assets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-VoidRay Assets Module
-Handles loading and management of game assets like images, sounds, and data files.
-"""
-
-from .asset_loader import AssetLoader
-
-__all__ = ['AssetLoader']
